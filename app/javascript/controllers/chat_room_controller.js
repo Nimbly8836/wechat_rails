0c95e28087e4d1e77af194ce9608eb7b6a8a0a90
leat <leat@leats-Air.lan>
2026-03-28 20:08:30 +0800

    Fix chat room message search escaping

--- app/javascript/controllers/chat_room_controller.js
+++ app/javascript/controllers/chat_room_controller.js
@@ -4255,4 +4255,13 @@ export default class extends Controller {
 
   }
 
+  escapeHtml(value) {
+    return String(value ?? "")
+      .replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/"/g, "&quot;")
+      .replace(/'/g, "&#39;");
+  }
+
 }
