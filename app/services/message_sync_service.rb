abd66f412b6f6b038bcc485f15e2f189eb198c9d
silly_abear <15930429+sillyAbear@user.noreply.gitee.com>
2026-04-08 21:58:27 +0800

    Revert "Improve realtime message delivery and chat room modularization"
    This reverts commit 230803e9ff1a293607384c0ff7fa074e89c9a1bd.

--- app/services/message_sync_service.rb
+++ app/services/message_sync_service.rb
@@ -29,7 +29,7 @@ class MessageSyncService
     payload
   end
 
-  def persist_payload(payload, full_backfill: false, inline_save: false)
+  def persist_payload(payload, full_backfill: false)
     saves = WechatModels::SyncMessageModel.parse_saves(normalize_payload(payload), sync_wxid)
     if saves.blank?
       BackfillMissingMessagesJob.perform_later(sync_wxid) if full_backfill
@@ -39,11 +39,7 @@ class MessageSyncService
     serialized_saves = saves.as_json
     synced_ids = saves.pluck(:id)
 
-    if inline_save
-      SaveChatRoomMessageJob.perform_now(serialized_saves, sync_wxid)
-    else
-      SaveChatRoomMessageJob.perform_later(serialized_saves, sync_wxid)
-    end
+    SaveChatRoomMessageJob.perform_later(serialized_saves, sync_wxid)
     SyncCreateContactsJob.perform_later(serialized_saves, sync_wxid)
     BackfillMissingMessagesJob.perform_later(sync_wxid, nil, synced_ids) if full_backfill
 
